4ed917b351e92c019b5cdc29d3f709809630fe86
tanvimeertops <tanvimeer.tops@gmail.com>
2024-09-11 11:12:45 +0530

    file handling

---
+++ python/advance/file-handling/main.py
@@ -0,0 +1,64 @@
+# open("example.txt", "w")
+
+# open("example1.txt", "x")
+
+# file = open("sample.txt", 'w')
+
+# file = open("sample.txt", 'a')
+
+# file.write(input("Enter something.... "))
+
+# file.close()
+
+# file = open("sample.txt", 'w')
+
+# print(file.read())
+# print(file.read(6))
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+# print(file.readline())
+
+# print(file.readlines())
+
+# lines = ['This is a line - 1\n', 'This is a line - 2\n', 'This is a line - 3\n', 'This is a line - 4\n', 'This is a line - 5\n', 'This is a new line added']
+
+# file.writelines(lines)
+# file.close()
+# print(file.read())
+
+
+# file = open("sample.txt", "w")
+# print(file.tell())
+# file.seek(6)
+# print(file.tell())
+# file.write("PHP")
+# file.close()
+
+import os
+
+# os.system("type nul > 2.txt")
+# os.system("py app.py")
+
+# os.mkdir("test")
+# os.rmdir("test")
+
+# os.remove('example.pdf')
+
+# file_name = input('Enter a file name with ext. : ')
+# os.system(f"type nul > {file_name}")
+
+# print(os.listdir(r'C:\Brijesh_Work\Batchs\22Jul_Python_Brijesh\python\advance\file-handling'))
+
+# num = 10
+# os.mkdir('musics')
+# for file in range(1, num+1):
+#     open("musics\\" + f"music-{file}.mp3", "w")
+
+# import uuid
+# os.system(f"type nul > {str(uuid.uuid4())}.mp3")
+
+import os
+print(dir(os))
